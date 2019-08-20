6483e3109c2c46e5e7a5e9f106b16b6d9c4491bd
wald <1000druid@gmail.com>
2019-08-20 22:10:45 +0200

    generation of users using celery

--- Celery/celery_with_django/mysite/mysite/celery.py
+++ Celery/celery_with_django/mysite/mysite/celery.py
@@ -4,5 +4,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 app = Celery('mysite')
+# namespace musi byc aby wiedziec w pliku settings ktore ustawienia dotycza Celery
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()

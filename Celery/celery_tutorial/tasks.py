4ade7ac2e177a28b884b1021fe22d73deaeaa28d
wald <1000druid@gmail.com>
2020-04-19 23:33:59 +0200

    add sleep to tasks.py

--- Celery/celery_tutorial/tasks.py
+++ Celery/celery_tutorial/tasks.py
@@ -1,9 +1,11 @@
 from celery import Celery
+from time import sleep
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery('tasks', broker='pyamqp://guest@localhost//', backend='db+postgresql://postgres:Test@720318@localhost/sandbox')
 
 @app.task
 def add(x, y):
+    sleep(10)
     return x + y
 
 
